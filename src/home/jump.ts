78b5499c7031de15143e6bc14dce285225657821
xiaozeng <420627029@qq.com>
2024-06-18 16:12:52 +0800

    2

---
+++ src/home/jump.ts
@@ -0,0 +1,9 @@
+if (data.scienFlag === "6") {
+    return <Rejection />;
+  }
+  if (firstOrAgain === "1") {
+    return <Home />;
+  }
+  if (firstOrAgain === "0") {
+    return <Relend />;
+  }
--- src/home/场景1和场景4.ts
+++ src/home/场景1和场景4.ts
@@ -1,4 +1,8 @@
 
+get 首页多场景-多应用
+firstOrAgain
+跳转页面
+
 // - 按钮
 get 申请执行自动借款流程-多应用
 to xx;
